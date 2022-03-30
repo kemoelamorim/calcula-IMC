5b306dd9abcffafa58e748ddd4847d3c47f9cee1
Kemoel Amorim Miranda <kemoel.amorim@gmail.com>
2022-03-30 14:57:33 -0300

    bootstrap add

--- js/principal.js
+++ js/principal.js
@@ -1,33 +1,47 @@
+var titulo = document.querySelector('.adicionar')
+console.log(titulo);
+var pacientes = document.querySelectorAll('.paciente')
 
-var paciente = document.querySelector("#primeiro-paciente")
-var tdPeso = paciente.querySelector(".info-peso")
-var tdAltura = paciente.querySelector(".info-altura")
-var tdImc = paciente.querySelector(".info-imc")
-
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
-
-var pesoEhValido = true;
-var alturaEhValida = true;
-
-if(peso <= 0 || peso >= 1000){
-  console.log("Peso inválido!");
-  pesoEhValido = false;
-  tdImc.textContent = "Peso inválido"
+pacientes.forEach(paciente => {
+  var tdNome = paciente.querySelector('.info-nome')
+  var nome = tdNome.textContent;
   
-}
-if(altura <= 0 || altura >= 3){
-  console.log("Altura inválida!");
-  tdImc.textContent = "Altura inválida";
-  alturaEhValida = false;
-}
+  var tdPeso = paciente.querySelector('.info-peso')
+  var peso = tdPeso.textContent;
+  
+  var tdAltura = paciente.querySelector('.info-altura')
+  var altura = tdAltura.textContent;
+  
+  var tdImc = paciente.querySelector('.info-imc');
+  
+  var pesoEhValido = true;
+  var alturaEhValida = true;
+  
+  if(peso <= 0 || peso >= 1000){
+    pesoEhValido = false;
+    tdImc.textContent = 'Peso inválido'
+    paciente.classList.add('paciente-invalido');
+    
+  }
+  if(altura <= 0 || altura >= 3){
+    tdImc.textContent = 'Altura inválida';
+    alturaEhValida = false;
+    paciente.classList.add('paciente-invalido');
+  }
+  
+  if(pesoEhValido && alturaEhValida){
+    var imc = peso / (altura * altura)
+    tdImc.textContent =  imc.toFixed(2)
+  }
+  
+  
+  console.log(peso)
+  console.log(altura) 
+  console.log(imc)
 
-if(pesoEhValido && alturaEhValida){
-  var imc = peso / (altura * altura)
-  tdImc.textContent =  imc 
-}
+});  
 
-
-console.log(peso)
-console.log(altura) console.log(imc)
-
+titulo.addEventListener('click', (event) => {
+  event.preventDefault()
+  console.log('Chamando uma arown function');
+})
